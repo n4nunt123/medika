4decbf0675e5b9d90290f494ea01364610141fbe
unknown <ihellostereo@gmail.com>
2022-11-20 10:08:32 +0700

    add navigators

---
+++ src/navigators/ContentScreen.js
@@ -0,0 +1,14 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import HistoryScreen from '../screens/HistoryScreen';
+
+const Tab = createBottomTabNavigator()
+
+export default function ContentScreen() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
+      <Tab.Screen name='History' component={HistoryScreen} options={{ headerShown: false }} />
+    </Tab.Navigator>
+  )
+}
