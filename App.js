dece615d92ff85f07f93515dc4dd457e12bb4b1a
unknown <ihellostereo@gmail.com>
2022-11-20 10:07:41 +0700

    second commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import MainNavigator from './src/navigators/MainNavigator'
+
+export default function App() {
+  return (
+    <>
+    <MainNavigator />
+    </>
+  );
+}
